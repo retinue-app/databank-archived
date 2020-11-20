e1bdc49144f6885336e2f252e0754f6b1611c600
Matan Lurey <matan@lurey.org>
2020-11-19 20:35:06 -0800

    Back to a good-ish place.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export const __intentionallyLeftBlankForNow = '';
+export { AttackDice } from './types/AttackDice';
+export { AttackPool } from './types/AttackPool';
+export { DefenseDice } from './types/DefenseDice';
+export { Faction, OfficialFaction } from './types/Faction';
+export { UnitCard } from './types/UnitCard';
+export { UnitRank } from './types/UnitRank';
+export { Trooper, UnitType, Vehicle } from './types/UnitType';
+export { OfficialUpgradeType, UpgradeType } from './types/UpgradeType';
+export { Weapon } from './types/Weapon';
