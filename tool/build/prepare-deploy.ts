49c35a65ad9673d67f9a63cc6300779096f4aa73
Matan Lurey <matan@lurey.org>
2020-11-24 18:16:11 -0800

    Tweak outputs.

--- tool/build/prepare-deploy.ts
+++ tool/build/prepare-deploy.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import shelljs from 'shelljs';
 
 shelljs.cp('package.json', path.join('dist', 'package.json'));
+shelljs.cp(path.join('src', 'data.json'), path.join('dist', 'data.json'));
 shelljs.cp('-R', path.join('src', 'data'), path.join('dist', 'data'));
 shelljs.cp('-R', path.join('src', 'schema'), path.join('dist', 'schema'));
 shelljs.cp('-R', path.join('src', 'types'), path.join('dist', 'types'));
